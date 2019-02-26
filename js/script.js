75dc92640fd6f84884f66298c4868a69a5a143d3
Khalil Bogle Thorpe <khalil.andall3@gmail.com>
2019-02-26 22:57:14 +0000

    Well Hello There

--- js/script.js
+++ js/script.js
@@ -2,15 +2,32 @@
 
 // BELOW Update the songs array with four of your favorites songs.
 var songs = [];
-// BELOW Add 4 More arrays to store images_links, artists, song lengths, and links for each song
-// Make sure they match the same order as your array above
+$("#add").click(function() {
+    
 
+
+
+var name =$("#song").val();
+var artist= $("#artist").val();
+var songLength =$("#length").val();
+var imageUrl =$("#image").val();
+var linkAddress =$("#link").val();
+
+songs.push({"name": name,
+            "artist": artist,
+            "length": songLength,
+            "image": imageUrl,
+            "address": linkAddress
+})
+console.log(songs);
+})
 
 
 function displaySongInfo(){
     // BELOW Use forEach Loop to display the data from each of your arrays in the correct div
-
-}
+songs.forEach(function(song)
+    {
+    $("#songs").append("<li>" + song.name + "</li>");
 
 function emptySongInfo(){
     $("#songs").empty();
